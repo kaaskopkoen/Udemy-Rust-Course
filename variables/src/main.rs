09669f0535a649d8a32a7d3c658272a699d76e77
Koen Oosterwijk <koen@oosterwijk.it>
2026-01-27 20:23:26 +0100

    rust-analyzer fixes

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -2,11 +2,11 @@ const STARTING_MISSILES :i32 = 8;
 const READY_AMOUNT :i32 = 2;
 
 fn main() {
-    let mut _missiles :i32 = STARTING_MISSILES;
+    let _missiles :i32 = STARTING_MISSILES;
     let _ready :i32 = READY_AMOUNT;
     println!("Firing {} of my {} missiles...", _ready, _missiles);
 
     //mut _missiles;
-    _missiles = _missiles - _ready;
+    let mut _missiles :i32 = _missiles - _ready;
     println!("{} missiles left", _missiles);
 }
