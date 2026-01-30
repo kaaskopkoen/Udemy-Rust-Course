e6fa034fcbd4635391c0acc066e7a823997e8c78
Koen Oosterwijk <koen@oosterwijk.it>
2026-01-30 18:53:34 +0100

    Done challenge K

--- k_enums/src/main.rs
+++ k_enums/src/main.rs
@@ -1,13 +1,72 @@
 // Silence some warnings that could distract from the exercise
 #![allow(unused)]
 
+fn inspect(value: Option<&str>) {
+    if let Some(s) = value {
+        println!("You passed in a {}", s);
+    }
+}
+
+enum Snack{
+    Apple,
+    Cookies(u8),
+    Sandwich{lettuce: bool, cheese: bool},
+    
+}
+
+trait Pricing{
+    fn price(self) -> u8;
+}
+
+trait AppleCheck{
+    fn is_apple(&self) -> bool;
+}
+
+impl AppleCheck for Snack{
+    fn is_apple(&self) -> bool{
+        let mut _return:bool = false;
+        match self {
+            Self::Apple => {_return = true},
+            Self::Cookies(amount) =>{},
+            Self::Sandwich { lettuce, cheese } =>{},
+        }
+        _return
+    }
+}
+
+impl Pricing for Snack{
+    fn price(self) -> u8{
+        let mut amount: u8 = 0;
+        match self {
+            Self::Apple => {amount += 5;},
+            Self::Cookies(AoC) => {amount += (AoC*2)},
+            Self::Sandwich { lettuce, cheese } => {
+                amount += 10;
+                if lettuce {
+                    amount += 1;
+                }if cheese {
+                    amount += 2;
+                }
+            },
+        }
+        amount
+    }
+}
+
 fn main() {
     // 1. If `maybe_fruit` below is a `Some` variant, then print out the string it wraps. Use the
     // Option type's `is_some` and `unwrap` methods with an `if` expression to implement the logic.
     // Then run your code. You should get the output "apple".
 
     let maybe_fruit: Option<&str> = Some("apple");
-    // if ...
+
+    if maybe_fruit.is_some(){
+        println!("this is {}", maybe_fruit.unwrap());
+    }
+
+    if let Some(x) = maybe_fruit{
+        println!("this is {}", x);
+    }
 
     // 2. Write a function `inspect` that accepts an `Option<&str>` as an argument and does not
     // return anything. Use an `if let` expression inside the function to get the value wrapped by
@@ -20,8 +79,8 @@ fn main() {
 
     let maybe_plant: Option<&str> = None;
     let maybe_food: Option<&str> = Some("cake");
-    // inspect(...);
-    // inspect(...);
+    inspect(maybe_plant);
+    inspect(maybe_food);
 
     // 3.  Write a loop that passes each number in the `numbers` vector to the `do_math` function
     // and then checks the result using a `match` expression.
@@ -37,7 +96,17 @@ fn main() {
     // You should get one error message with a sad face, and one line with the number 100.
 
     let numbers = vec![0, 1];
-    // for ...
+    for number in numbers{
+        let res = do_math(number);
+        match res{
+            Ok(f) => {
+                println!("The result was {}", f);
+            },
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }        
+    }
 
     // 4. Define an enum named `Snack` with the following variants:
     //
@@ -49,26 +118,26 @@ fn main() {
     // Then uncomment and run the code below. If you defined the enum correctly, you should get
     // output about three snacks.
 
-    // let healthy_snack = Snack::Apple;
-    // let sugary_snack = Snack::Cookies(18);
-    // let lunch = Snack::Sandwich {
-    //     lettuce: false,
-    //     cheese: true,
-    // };
-    // if let Snack::Apple = healthy_snack {
-    //     println!("The healthy snack is an apple.");
-    // }
-    // if let Snack::Cookies(num_cookies) = sugary_snack {
-    //     println!("The sugary snack is {} cookies", num_cookies);
-    // }
-    // if let Snack::Sandwich { lettuce, cheese } = lunch {
-    //     let lettuce_msg = if lettuce { "does" } else { "does not" };
-    //     let cheese_msg = if cheese { "does" } else { "does not" };
-    //     println!(
-    //         "The sandwich {} have lettuce and {} have cheese.",
-    //         lettuce_msg, cheese_msg
-    //     );
-    // }
+    let healthy_snack = Snack::Apple;
+    let sugary_snack = Snack::Cookies(18);
+    let lunch = Snack::Sandwich {
+        lettuce: false,
+        cheese: true,
+    };
+    if let Snack::Apple = healthy_snack {
+        println!("The healthy snack is an apple.");
+    }
+    if let Snack::Cookies(num_cookies) = sugary_snack {
+        println!("The sugary snack is {} cookies", num_cookies);
+    }
+    if let Snack::Sandwich { lettuce, cheese } = lunch {
+        let lettuce_msg = if lettuce { "does" } else { "does not" };
+        let cheese_msg = if cheese { "does" } else { "does not" };
+        println!(
+            "The sandwich {} have lettuce and {} have cheese.",
+            lettuce_msg, cheese_msg
+        );
+    }
 
     // 5. Create an `impl` block for the `Snack` enum and implement a method named `price` which
     // takes ownership of a Snack and returns a u8 representing the price of the snack according to
@@ -83,33 +152,33 @@ fn main() {
     // Then uncomment and run the code below. You should see three lines ending with the costs of
     // $5, $36, and $12.
 
-    // println!("An apple costs ${}", healthy_snack.price());
-    // if let Snack::Cookies(number) = sugary_snack {
-    //     println!("{} cookies costs ${}", number, sugary_snack.price());
-    // }
-    // if let Snack::Sandwich { lettuce, cheese } = lunch {
-    //     let lettuce_message = if lettuce { " with lettuce" } else { "" };
-    //     let cheese_message = if cheese { " with cheese" } else { "" };
-    //     println!(
-    //         "A sandwich{}{} costs ${}",
-    //         lettuce_message,
-    //         cheese_message,
-    //         lunch.price()
-    //     );
-    // }
+    println!("An apple costs ${}", healthy_snack.price());
+    if let Snack::Cookies(number) = sugary_snack {
+        println!("{} cookies costs ${}", number, sugary_snack.price());
+    }
+    if let Snack::Sandwich { lettuce, cheese } = lunch {
+        let lettuce_message = if lettuce { " with lettuce" } else { "" };
+        let cheese_message = if cheese { " with cheese" } else { "" };
+        println!(
+            "A sandwich{}{} costs ${}",
+            lettuce_message,
+            cheese_message,
+            lunch.price()
+        );
+    }
 
     // Challenge 1: Implement an `is_apple` method for Snack that return a bool. Return `true` if
     // the value is an `Apple` variant, and `false` otherwise. Then uncomment and run the code
     // below.
 
-    // let snacks = vec![Snack::Apple, Snack::Cookies(5), Snack::Apple];
-    // for (index, snack) in snacks.iter().enumerate() {
-    //     if snack.is_apple() {
-    //         println!("Snack {} is an apple.", index)
-    //     } else {
-    //         println!("Snack {} is NOT an apple.", index)
-    //     }
-    // }
+    let snacks = vec![Snack::Apple, Snack::Cookies(5), Snack::Apple];
+    for (index, snack) in snacks.iter().enumerate() {
+        if snack.is_apple() {
+            println!("Snack {} is an apple.", index)
+        } else {
+            println!("Snack {} is NOT an apple.", index)
+        }
+    }
 
     // Challenge 2: Refactor the code from (4) to put all off the variables into a vector, then loop
     // through the vector and use a `match` expression instead of `if let` statements. The output
